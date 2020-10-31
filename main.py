f248294962559e63a52ae8f46fa45c0aef1a0a21
Thatoddone1 <techguyfun@gmail.com>
2020-10-31 08:43:53 -0400

    added a way to togle different types of charecters and added punc charecters

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ import random
 import string
 times = input('Enter The Nuber Of Password(s) You Want To Genarate: ')
 length = input('Enter The Length Of the Password(s): ')
-wantLower = input('Do you want lower case letters in the password(s)')
-wantUpper = input('Do you want upper case lestters in the password(s)')
-wantNum = input('Do you want numbers in the password(s)')
-
+wantLower = input('Do you want lower case letters in the password(s) (y/n)')
+wantUpper = input('Do you want upper case lestters in the password(s) (y/n)')
+wantNum = input('Do you want numbers in the password(s) (y/n)')
+wantPunc = input('Do you want punctuation in the password(s (y/n))')
 
 
 b = 0
 i = 0
-c = random.randint(1,3)
+c = random.randint(1,4)
 while b < int(times):
     while i < int(length):
-        if c == 1:
+        if c == 1 and wantLower == 'y':
             print(random.choice(string.ascii_lowercase), end='')
-        elif c == 2:
+            i = i+1
+        elif c == 2 and wantNum == 'y':
             print(random.randint(1,9), end='')
-        elif c == 3:
+            i = i+1
+        elif c == 3 and wantUpper == 'y':
             print(random.choice(string.ascii_uppercase), end='')
-        i = i+1
-        c = random.randint(1,3)
+            i = i+1
+        elif c == 4 and wantPunc == 'y':
+            print(random.choice(string.punctuation), end='')
+            i = i+1
+        c = random.randint(1,4)
             
     print('')
     i = 0
