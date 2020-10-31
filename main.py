57875312c723521c03c8db0e6e3a3b3210e7d3cb
Thatoddone1 <techguyfun@gmail.com>
2020-10-31 07:46:28 -0400

    added a readme.md file

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import random
 import string
+times = input('Enter The Nuber Of Password(s) You Want To Genarate: ')
+length = input('Enter The Length Of the Password(s): ')
 
+b = 0
 i = 0
-while i < 10:
-    print(random.choice(string.ascii_letters), end='')
-    i = i+1
-print('')
+while b < int(times):
+    while i < int(length):
+        if random.randint(1,2) == 1:
+            print(random.choice(string.ascii_letters), end='')
+            i = i+1
+        else:
+            print(random.randint(1,9), end='')
+            i = i+1
+    print('')
+    i = 0
+    b = b+1
+    
